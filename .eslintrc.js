21c4a56d847dfd3f7a4eda148982b98212bf7de6
lee <lee@dcodegroup.com>
2022-07-27 16:24:11 +1000

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,41 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential"
+  "env": {
+    "browser": true,
+    "es2021": true,
+  },
+  "extends": [
+    "plugin:vue/base",
+    "plugin:vue/essential",
+    "plugin:vue/strongly-recommended",
+    "plugin:vue/recommended",
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+  },
+  "plugins": [
+    "vue",
+  ],
+  "rules": {
+    "vue/no-mutating-props": "off",
+    "vue/html-self-closing": "off",
+    "comma-dangle": ["error", {
+      "objects": "always-multiline",
+      "arrays": "always-multiline",
+    }],
+    "indent": [2, 2, { "SwitchCase": 1 }],
+    "object-curly-spacing": [
+      "error",
+      "always",
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
+    "space-before-function-paren": [
+      "error",
+      "never",
     ],
-    "rules": {
-    }
+    "space-before-blocks": [
+      "error",
+      "always",
+    ],
+    "vue/no-v-html": "off",
+  },
 }
